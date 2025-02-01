1c87047819dc97816f46d762b9c2957e73e69698
Mario Pavlov <marioppavlov@gmail.com>
2025-02-01 08:14:45 +0200

    Check for Command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,9 +10,32 @@ import (
 var _ = fmt.Fprint
 
 func main() {
+	commands := make(map[string] string)
+
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	userInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	
+	// Trim newline
+	userInput = userInput[:len(userInput) - 1]
+
+	if err != nil {
+		fmt.Println("Error: " + err.Error())
+		return
+	}
+
+	// Get Command
+	command, exists := commands[userInput]
+	if !exists {
+		command = userInput
+	}
+
+	if exists {
+		fmt.Println(command + ": to be executed")
+	} else {
+		fmt.Println(command + ": command not found")
+	}
+
 }
