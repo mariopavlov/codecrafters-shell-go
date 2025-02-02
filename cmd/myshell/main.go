5c5bac05ddc129df0b60e597c008c78598b68211
Mario Pavlov <marioppavlov@gmail.com>
2025-02-02 16:46:39 +0200

    Implement External Commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
-
 
 func main() {
 
@@ -16,21 +17,39 @@ func main() {
 		// Wait for user input
 		rawInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		userInput := strings.Split(TrimNewLine(rawInput), " ")
+		userCommand := userInput[0]
 
 		if err != nil {
 			fmt.Println("Error reading input: " + err.Error())
 			os.Exit(1)
 		}
 
-		// Get Command
 		command, exists := GetCommand(userInput[0])
 		if exists {
 			command.Handler(userInput)
+		} else if commandPath, exists := SearchCommandPath(userCommand); exists {
+			ExecuteExternalCommand(commandPath, userInput[1:])
 		} else {
 			fmt.Println(userInput[0] + ": command not found")
 		}
 	}
 
+}
+
+func ExecuteExternalCommand(path string, args []string) {
+	command := exec.Command(path, args...)
+
+	var outBuffer bytes.Buffer
+	command.Stdout = &outBuffer
+	command.Stdout = os.Stdout
+	command.Stdin = os.Stdin
+	command.Stderr = os.Stderr
+
+	if err := command.Run(); err != nil {
+		fmt.Println("Error executing command:", err)
+	}
+
+	fmt.Print(outBuffer.String())
 }
 
 func TrimNewLine(prompt string) string {
