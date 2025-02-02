1042b152b914a6faab6c110148c1b0e350aa3644
Mario Pavlov <marioppavlov@gmail.com>
2025-02-02 11:08:46 +0200

    Refactor: Cleaning Main

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,53 +7,8 @@ import (
 	"strings"
 )
 
-type Command struct {
-	Name string
-	Description string
-	Usage string
-	Handler func(args []string) error
-}
-
-func commandExit(args []string) error {
-	if len(args) <= 1 {
-		fmt.Println("Error: Exit requires exit code ")
-		os.Exit(1)
-	}
-
-	if args[1] == "0" {
-		os.Exit(0)
-	} else {
-		fmt.Println("Error: Exit with code " + args[1])
-		os.Exit(1)
-	}
-
-	return nil
-}
-
-func commandEcho(args []string) error {
-
-	echoText := strings.Join(args[1:], " ")
-	fmt.Println(args)
-	fmt.Println(echoText)
-
-	return nil
-}
 
 func main() {
-	commands := map[string] Command {
-		"exit": {
-			Name: "exit",
-			Description: "exit is a shell builtin",
-			Usage: "exit [code]",
-			Handler: commandExit,
-		},
-		"echo": {
-			Name: "echo",
-			Description: "echo is a shell builtin",
-			Usage: "echo [text to print out]",
-			Handler: commandEcho,
-		},
-	}
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
@@ -68,7 +23,7 @@ func main() {
 		}
 
 		// Get Command
-		command, exists := commands[userInput[0]]
+		command, exists := GetCommand(userInput[0])
 		if exists {
 			command.Handler(userInput)
 		} else {
