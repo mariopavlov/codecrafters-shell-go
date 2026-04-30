37da0011bc8490104354fe5da294d904da7c07fc
Mario Pavlov <marioppavlov@gmail.com>
2026-04-30 16:07:02 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -11,18 +11,21 @@ import (
 var _ = fmt.Print
 
 func main() {
-	fmt.Print("$ ")
-
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-	}
-
-	command = strings.TrimSpace(command)
+	command := readCommand()
 
 	switch command {
 	case "cd":
 	default:
 		fmt.Printf("%s: command not found\n", command)
 	}
+}
+
+func readCommand() string {
+	fmt.Print("$ ")
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	}
+
+	return strings.TrimSpace(command)
 }
